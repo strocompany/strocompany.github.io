bc456800aa5fca8e0d057f49bdb7483199b269dc
karmdesai <karmdesai18@gmail.com>
2019-02-13 12:43:35 -0500

    added contact form.

--- js/fadeContent.js
+++ js/fadeContent.js
@@ -7,9 +7,7 @@ $(window).on("load",function() {
 
       /* If the element is completely within bounds of the window, fade it in */
       if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-        if ($(this).css("opacity")==0) {$(this).fadeTo(700,1);}
-      } else { //object goes out of view (scrolling up)
-        if ($(this).css("opacity")==1) {$(this).fadeTo(700,0);}
+        $(this).animate({'opacity':'1'},700);
       }
     });
   }).scroll(); //invoke scroll-handler on page-load
